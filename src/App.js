62cc4bcdff039e4f1f2b4a3640ad81ea6aa3e671
hoai_bao <hoaibao572@gmail.com>
2022-11-02 16:09:53 +0700

    update folder project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import AdminHome from './pages/Admins/AdminHome';
+
+export const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/admin" component={AdminHome} />
+        {/* Khi không có trang cụ thể thì mặc định địa chỉ */}
+      </Switch>
+    </Router>
   );
 }
 
