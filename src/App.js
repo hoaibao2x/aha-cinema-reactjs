76d89cb80d4efa43f38631f7aea1e5912ac4b577
Y <tominhy77@gmail.com>
2022-11-09 01:59:38 +0700

    create-branch-Y-user-home-page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import FilmsList from './pages/Admins/QL.Films/FilmsList';
 
 export const history = createBrowserHistory();
 
 function App() {
   return (
-    <FilmsList />
-    // <Router history={history}>
-    //   <Switch>
-    //     {/* User Route */}
+    <Router history={history}>
+      <Switch>
 
-
-    //     {/* Admin Route */}
-
-
-    //     {/* Default Route */}
-    //   </Switch>
-    // </Router>
+      </Switch>
+    </Router>
   );
 }
 
